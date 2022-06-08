11bd6a4bdc42ade3ef1cf1b6b90e2931e9b84e09
VeeNationWide <102980506+VeeNationWide@users.noreply.github.com>
2022-06-08 18:07:03 +0200

    Add project files.

---
+++ u19072237HW3/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace u19072237HW3.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+        public ActionResult Index()
+        {
+             return View();
+           
+        }
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file, string Type)
+        {
+            try
+            {
+                if (file != null && file.ContentLength > 0)
+                {
+                    string filename = Path.GetFileName(file.FileName);
+                    string filepath = Path.Combine(Server.MapPath("/Media/" + Type), filename);
+                    file.SaveAs(filepath);
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return RedirectToAction("Index");
+            }
+
+        }
+
+
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View("About");
+        }
+
+       
+    }
+}
---
+++ u19072237HW3/Models/FileModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace u19072237HW3.Models
+{
+    public class FileModel
+    {
+        [Display(Name = "File Name")]
+        public string FileName { get; set; }
+
+        [Required(ErrorMessage = "Please select file.")]
+        [Display(Name = "Browse File")]
+        public HttpPostedFileBase[] Files { get; set; }
+
+    }
+}
